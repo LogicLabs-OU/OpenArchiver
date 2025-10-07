68ddda58065be2352294cd537f5e21944be3f86c
zophiana <53372671+zophiana@users.noreply.github.com>
2025-10-07 22:02:46 +0200

    add proper type for bullmq config

--- packages/backend/src/config/redis.ts
+++ packages/backend/src/config/redis.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
+import { type ConnectionOptions } from 'bullmq';
 
 /**
  * @see https://github.com/taskforcesh/bullmq/blob/master/docs/gitbook/guide/connections.md
  */
-const connectionOptions: any = {
+const connectionOptions: ConnectionOptions = {
 	host: process.env.REDIS_HOST || 'localhost',
 	port: (process.env.REDIS_PORT && parseInt(process.env.REDIS_PORT, 10)) || 6379,
 	password: process.env.REDIS_PASSWORD,
